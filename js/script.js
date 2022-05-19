7289f57504c9515f7189c41457a1e20c32343b95
Joseph Kwon <99274059+JosephK2006@users.noreply.github.com>
2022-05-19 02:41:43 +0000

    updated index.html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 "use strict"
 
-// Calculate what museum admission fee you will need to pay depending on day and age
+// This function displays all numbers from user starting number to the user ending number
 function enterClicked () {
-  	// initialize variables
+  // get the user starting number 
 	let counter = parseInt(document.getElementById('number').value)
-
+  // initialize numbers as an empty string 
   let numbers = ""
-
+  // get the user ending number 
   let userNum = parseInt(document.getElementById('number1').value)
 
 
-
+  // use a while loop to display the numbers from user starting number up to the user ending number 
   while (counter <= userNum) {
+    // build the string of numbers with a line break each time 
     numbers = numbers + counter + "<br>"
-
+    // increment the counter
     counter = counter + 1
   }
 
